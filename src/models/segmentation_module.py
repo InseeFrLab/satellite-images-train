76f7d32f3fb42c6241f9d33dea19ff718e054b76
ThomasFaria <thomas.faria@ensae.fr>
2023-12-11 13:29:19 +0000

    add files

---
+++ src/models/segmentation_module.py
@@ -0,0 +1,123 @@
+"""
+"""
+from typing import Dict, Union
+
+import pytorch_lightning as pl
+from classes.optim.evaluation_model import calculate_IOU
+from torch import nn, optim
+
+
+class SegmentationModule(pl.LightningModule):
+    """
+    Pytorch Lightning Module for DeepLabv3.
+    """
+
+    def __init__(
+        self,
+        model: nn.Module,
+        loss: Union[nn.Module],
+        optimizer: Union[optim.SGD, optim.Adam],
+        optimizer_params: Dict,
+        scheduler: Union[optim.lr_scheduler.OneCycleLR, optim.lr_scheduler.ReduceLROnPlateau],
+        scheduler_params: Dict,
+        scheduler_interval: str,
+    ):
+        """
+        Initialize TableNet Module.
+        Args:
+            model
+            loss
+            optimizer
+            optimizer_params
+            scheduler
+            scheduler_params
+            scheduler_interval
+        """
+        super().__init__()
+
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+        self.optimizer_params = optimizer_params
+        self.scheduler = scheduler
+        self.scheduler_params = scheduler_params
+        self.scheduler_interval = scheduler_interval
+
+    def forward(self, batch):
+        """
+        Perform forward-pass.
+        Args:
+            batch (tensor): Batch of images to perform forward-pass.
+        Returns (Tuple[tensor, tensor]): Table, Column prediction.
+        """
+        return self.model(batch)
+
+    def training_step(self, batch, batch_idx):
+        """
+        Training step.
+        Args:
+            batch (List[Tensor]): Data for training.
+            batch_idx (int): batch index.
+        Returns: Tensor
+        """
+        images, labels, dic = batch
+
+        output = self.forward(images)
+        loss = self.loss(output, labels)
+
+        self.log("train_loss", loss, on_epoch=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        """
+        Validation step.
+        Args:
+            batch (List[Tensor]): Data for training.
+            batch_idx (int): batch index.
+        Returns: Tensor
+        """
+        images, labels, dic = batch
+
+        output = self.forward(images)
+        loss = self.loss(output, labels)
+        IOU = calculate_IOU(output, labels)
+
+        self.log("validation_IOU", IOU, on_epoch=True)
+        self.log("validation_loss", loss, on_epoch=True)
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        """
+        Test step.
+        Args:
+            batch (List[Tensor]): Data for training.
+            batch_idx (int): batch index.
+        Returns: Tensor
+        """
+        images, labels, dic = batch
+        output = self.forward(images)
+
+        loss = self.loss(output, labels)
+        self.log("test_loss", loss, on_epoch=True)
+
+        IOU = calculate_IOU(output, labels)
+        self.log("test IOU", IOU, on_epoch=True)
+
+        return IOU
+
+    def configure_optimizers(self):
+        """
+        Configure optimizer for pytorch lighting.
+        Returns: optimizer and scheduler for pytorch lighting.
+        """
+        optimizer = self.optimizer(self.parameters(), **self.optimizer_params)
+        scheduler = self.scheduler(optimizer, self.scheduler_params["mode"])
+        scheduler = {
+            "scheduler": scheduler,
+            "monitor": self.scheduler_params["monitor"],
+            "interval": self.scheduler_interval,
+        }
+
+        return [optimizer], [scheduler]
---
+++ src/optim/metrics.py
@@ -0,0 +1,25 @@
+import torch
+
+
+def calculate_IOU(output, labels):
+    """
+    Calculate Intersection Over Union indicator
+    based on output segmentation mask of a model
+    and the true segmentations mask
+
+    Args:
+        output: the output of the segmentation
+        label: the true segmentation mask
+
+    """
+    preds = torch.argmax(output, axis=1)
+
+    numIOU = torch.sum((preds * labels), axis=[1, 2])  # vaut 1 si les 2 = 1
+    denomIOU = torch.sum(torch.clamp(preds + labels, max=1), axis=[1, 2])
+
+    IOU = numIOU / denomIOU
+    IOU = torch.tensor(
+        [1 if torch.isnan(x) else x for x in IOU], dtype=torch.float
+    )  # TODO: pourquoi on peut obtenir des NaN
+
+    return torch.mean(IOU)
