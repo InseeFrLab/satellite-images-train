cd2dbb5e42fda3dce1094ef280529d69a52ccb08
tomseimandi <tom.seimandi@gmail.com>
2024-02-02 10:48:27 +0000

    Add clarification comment for the IOU fn

--- src/optim/metrics.py
+++ src/optim/metrics.py
@@ -4,12 +4,13 @@ import torch
 def IOU(output, labels):
     """
     Calculate Intersection Over Union indicator
+    for the positive class of a segmentation task
     based on output segmentation mask of a model
-    and the true segmentations mask
+    and the true segmentations mask.
 
     Args:
-        output: the output of the segmentation
-        label: the true segmentation mask
+        output: Output of the segmentation model.
+        label: True segmentation mask.
 
     """
     preds = torch.argmax(output, axis=1)
@@ -20,6 +21,9 @@ def IOU(output, labels):
     IOU = numIOU / denomIOU
     IOU = torch.tensor(
         [1 if torch.isnan(x) else x for x in IOU], dtype=torch.float
-    )  # TODO: pourquoi on peut obtenir des NaN
+    )  # TODO: ici si on a des images sans aucun pixel bâtiment
+    # on "biaise" potentiellement l'IOU vers le haut, parce que si
+    # on prédit rien on a une IOU de 1. N'arrive pas si on
+    # n'a pas d'images sans pixel bâtiment
 
     return torch.mean(IOU)
